de2545afa4828d8704f995c8c03e553ebe853f00
Ismauri <imcastillocastro@gmail.com>
2026-08-01 20:44:59 -0600

    nueva clase de modelo 'Producto', ejemplos de uso de states para ver detalles, restar stock y agregar stock de producto

--- lib/main.dart
+++ lib/main.dart
@@ -1,29 +1,21 @@
 import 'package:flutter/material.dart';
+import 'screens/home_page.dart';
 
 void main() {
   runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
+
   const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'Gestor de Inventario',
-      theme: ThemeData.dark(),
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text('Gestor de Inventario'),
-        ),
-        body: Center(
-          child: Text('¡Bienvenido al Gestor de Inventario!'),
-        ),
-        floatingActionButton: FloatingActionButton(
-          onPressed: () {},
-          child: Icon(Icons.add),
-        ),
-      )
+      home: HomePage()
     );
   }
 }
+
