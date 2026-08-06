54ebc21df1761c07b6a03818796812848fc0f98a
Ismauri <imcastillocastro@gmail.com>
2026-08-06 09:29:47 -0600

    Configuracion de estilos de la aplicacion

--- lib/config/app_text_styles.dart
+++ lib/config/app_text_styles.dart
@@ -2,47 +2,52 @@ import 'package:flutter/material.dart';
 import 'app_colors.dart';
 
 class AppTextStyles {
-  // AppBar title
+  // titulo del appbar
   static const TextStyle screenTitle = TextStyle(
     fontSize: 22,
     fontWeight: FontWeight.bold,
     color: Colors.white,
   );
 
-  // Section titles
+  // titulos por seccion
   static const TextStyle sectionTitle = TextStyle(
     fontSize: 18,
     fontWeight: FontWeight.bold,
     color: Colors.black87,
   );
 
-  // Card titles
+  // titulo de cards
   static const TextStyle cardTitle = TextStyle(
     fontSize: 16,
     fontWeight: FontWeight.w600,
     color: Colors.black87,
   );
 
-  // Secondary text
+  // textos secundarios
   static const TextStyle subtitle = TextStyle(
     fontSize: 14,
     color: Colors.black54,
   );
 
-  // Statistics
+    static const TextStyle subtitle2 = TextStyle(
+    fontSize: 14,
+    color: Colors.white70,
+  );
+
+  // estadisticas
   static const TextStyle statistic = TextStyle(
     fontSize: 34,
     fontWeight: FontWeight.bold,
   );
 
-  // Prices
+  // precios
   static const TextStyle price = TextStyle(
     fontSize: 15,
     fontWeight: FontWeight.w600,
     color: AppColors.success,
   );
 
-  // Buttons
+  // botoenes
   static const TextStyle button = TextStyle(
     fontSize: 15,
     fontWeight: FontWeight.w600,
