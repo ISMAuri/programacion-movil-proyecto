82ea7d8dc634c40964aee5e02daf15ef65f55f7b
Samy <samanthaenamoradoty@gmail.com>
2026-08-10 23:42:38 -0600

    pantalla para crear y editar productos

--- lib/screens/listado_productos_screen.dart
+++ lib/screens/listado_productos_screen.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import '../config/app_text_styles.dart';
 import '../config/app_colors.dart';
 import '../widgets/search_bar_widget.dart';
+import 'agregar_producto_screen.dart';
 
 class ListadoProductosScreen extends StatelessWidget {
   const ListadoProductosScreen({super.key});
@@ -21,10 +22,13 @@ class ListadoProductosScreen extends StatelessWidget {
         foregroundColor: AppColors.white,
       ),
       backgroundColor: AppColors.background,
-      body: SearchBarWidget(), // Aquí se utiliza el widget de la barra de búsqueda
+      body: SearchBarWidget(), 
       floatingActionButton: FloatingActionButton(
         onPressed: () {
-          // Acción al presionar el botón
+          Navigator.push(
+            context,
+            MaterialPageRoute(builder: (context) => const AgregarProductoScreen()),
+          );
         },
         backgroundColor: AppColors.secondary,
         foregroundColor: AppColors.white,
